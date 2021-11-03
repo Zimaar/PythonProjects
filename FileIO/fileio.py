a3f533527772a08011d7bc4e4b408092abf014ec
Zimaar <raamiz2k1@gmail.com>
2021-11-03 14:18:00 +0500

    A File I/O Script that creates a file, writes data to it, and reverses each line in the file

---
+++ FileIO/fileio.py
@@ -0,0 +1,19 @@
+
+with open("new_file.txt","w+") as new_file: #creating a file called new_file.txt and opening it in write mode
+    new_file.write("dog\n")
+    new_file.write("can\n")
+    new_file.write("parrot\n")
+    new_file.write("hamster\n")
+    new_file.write("mouse\n")               #writing a list of animals to fill the empty txt file
+
+with open("new_file.txt","r") as new_file:
+    data = new_file.read() 
+    reversed_data = data[::-1] #reading the contents to a variable, and reversing the contents via string splicing
+
+with open("new_file.txt","w") as new_file: #writes the reversed contents back into new_file
+    new_file.writelines(reversed_data)
+    new_file.close()
+
+
+
+    
