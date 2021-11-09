0232edd7da33ae47fb20893daabdd79015a758d0
Zimaar <raamiz2k1@gmail.com>
2021-11-09 02:25:04 -0800

    The contents before/after the file is changed are now printed

--- FileIO/fileio.py
+++ FileIO/fileio.py
@@ -1,3 +1,9 @@
+def print_file(): # prints the file. I need to print it more than once so I create a function to prevent repeated code
+    new_file = open("new_file.txt")
+    lines = new_file.readlines()
+    for line in lines:
+        print(line)
+    new_file.close()
 
 with open("new_file.txt","w+") as new_file: #creating a file called new_file.txt and opening it in write mode
     new_file.write("dog\n")
@@ -6,6 +12,8 @@ with open("new_file.txt","w+") as new_file: #creating a file called new_file.txt
     new_file.write("hamster\n")
     new_file.write("mouse\n")               #writing a list of animals to fill the empty txt file
 
+print_file()
+
 with open("new_file.txt","r") as new_file:
     data = new_file.read() 
     reversed_data = data[::-1] #reading the contents to a variable, and reversing the contents via string splicing
@@ -13,6 +21,8 @@ with open("new_file.txt","r") as new_file:
 with open("new_file.txt","w") as new_file: #writes the reversed contents back into new_file
     new_file.writelines(reversed_data)
     new_file.close()
+
+print_file()
 
 
 
