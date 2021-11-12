e6c194799efed4013933b9956edaf90bb8ee42c8
Zimaar <raamiz2k1@gmail.com>
2021-11-11 22:22:34 -0800

    Now able to search for countries with more than one word in the name, added comments

--- SearchString/searchstring.py
+++ SearchString/searchstring.py
@@ -1,14 +1,14 @@
 with open("sample.txt",'r') as f:
-    string = input("What country are you looking for? ")
-    string = string.strip().capitalize()
-    count = 0
+    string = input("What country are you looking for? ") # asks for user input
+    string = string.lower() # makes input lowercase, since txt is all lowercase
+    count = 0 # starts a counter to print line #
 
     for line in f:
-        line = line.rstrip()
+        line = line.rstrip() # removes all traling characters
         count += 1
 
         if string == line:
             print("Found " + line + " at line #" + str(count))
-            break
+            break # breaks if country is found
     else:
-        print("Invalid Input")
+        print("Invalid Input") # exception if user entered incorrectly
