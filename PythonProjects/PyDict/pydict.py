16cac7789341faabaa394a5ef1d38e7aa465e880
Zimaar <raamiz2k1@gmail.com>
2021-12-20 23:56:59 -0800

    Added comments

--- PythonProjects/PyDict/pydict.py
+++ PythonProjects/PyDict/pydict.py
@@ -1,6 +1,6 @@
 from nltk.corpus import wordnet
 
-meaning = input("Enter one word to find the meaning: ")
-syns = wordnet.synsets(meaning)
-print(syns[0].definition())
+word = input("Enter one word to find the meaning: ") #getting user input
+syns = wordnet.synsets(word) #using wordnet module to find related synonym set
+print(syns[0].definition()) #extracting definition of synonym set and printing
 
